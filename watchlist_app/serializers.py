b63a6515d4ea27049a8da45263387926c9f17dfe
M.Asad Ali Akbar <m.asad@bitswits.com>
2022-12-30 12:26:52 +0500

    put,delete

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -3,10 +3,20 @@ from .models import *
 
 
 class MovieSerializers(serializers.Serializer):
-    name = serializers.CharField(max_length=50) 
-    description = serializers.CharField(max_length=200)
-    active = serializers.BooleanField(default=True)
+    name = serializers.CharField(required=False, max_length=50) 
+    description = serializers.CharField(required=False,max_length=200)
+    active = serializers.BooleanField(required=False,default=True)
     
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active',instance.active)
+        instance.save()
+        return instance
     # class Meta:
     #     model = Movie
-    #     fields = ('id', 'name', 'description' ,'active')
+    #     fields = '__all__'
+        # fields = ('id', 'name', 'description' ,'active')
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from .serializers import MovieSerializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 
 def movie_list(request):
     movies = Movie.objects.all()
@@ -23,14 +23,39 @@ def movie_details(request, pk):
     }
     return JsonResponse(data)
 
-@api_view(['GET'])
-def serial_movie_list(request):
-    movies = Movie.objects.all()
-    serializers = MovieSerializers(movies,many=True)
-    return Response(serializers.data)
+@api_view(['GET','POST'])
+def serial_movie_list(request): 
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializers = MovieSerializers(movies,many=True)
+        return Response(serializers.data,status=status.HTTP_201_CREATED)
+    if request.method == 'POST':
+        serializers = MovieSerializers(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['GET'])
+
+@api_view(['GET','PUT','DELETE'])
 def serial_movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializers = MovieSerializers(movie)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk=pk)
+            serializers = MovieSerializers(movie)
+            return Response(serializers.data,status=status.HTTP_200_OK)
+        except :
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializers = MovieSerializers(movie,data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
