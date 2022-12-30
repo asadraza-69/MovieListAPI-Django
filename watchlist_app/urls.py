794411ecbab51e473f4c38e237968cfc9807a99c
M.Asad Ali Akbar <m.asad@bitswits.com>
2022-12-30 12:53:24 +0500

    class based view

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -3,8 +3,10 @@ from watchlist_app.views import *
 
 
 urlpatterns = [
-    path('', movie_list, name='movie-list'),
-    path('<int:pk>', movie_details, name='movie-detail'),
-    path('serializer/', serial_movie_list, name='serial_movie_list'),
-    path('serializer/<int:pk>', serial_movie_details, name='serial_movie_details'),
+    path('', movie_list, name='movie_list'),
+    path('<int:pk>', movie_details, name='movie_details'),
+    path('func-serializer/', serial_movie_list, name='serial_movie_list'),
+    path('func-serializer/<int:pk>', serial_movie_details, name='serial_movie_details'),
+    path('serializer/', MovielistApiView.as_view(), name='MovielistApiView'),
+    path('serializer/<int:pk>', MovieDetailApiView.as_view(), name='MovieDetailApiView'),
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -5,7 +5,42 @@ from .serializers import MovieSerializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
+class MovielistApiView(APIView):
+    def get(self,request):
+        movies = Movie.objects.all()
+        serializers = MovieSerializers(movies,many=True)
+        return Response(serializers.data,status=status.HTTP_201_CREATED)
+    def post(self,request):
+        serializers = MovieSerializers(data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class MovieDetailApiView(APIView):
+    def get(self,request,pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+            serializers = MovieSerializers(movie)
+            return Response(serializers.data,status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def put(self,request,pk):
+        movie = Movie.objects.get(pk=pk)
+        serializers = MovieSerializers(movie,data = request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def delete(self,request,pk):
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 def movie_list(request):
     movies = Movie.objects.all()
     data = {
@@ -23,12 +58,15 @@ def movie_details(request, pk):
     }
     return JsonResponse(data)
 
+
 @api_view(['GET','POST'])
 def serial_movie_list(request): 
+    
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializers = MovieSerializers(movies,many=True)
         return Response(serializers.data,status=status.HTTP_201_CREATED)
+    
     if request.method == 'POST':
         serializers = MovieSerializers(data = request.data)
         if serializers.is_valid():
@@ -40,13 +78,15 @@ def serial_movie_list(request):
 
 @api_view(['GET','PUT','DELETE'])
 def serial_movie_details(request, pk):
+    
     if request.method == 'GET':
         try:
             movie = Movie.objects.get(pk=pk)
             serializers = MovieSerializers(movie)
             return Response(serializers.data,status=status.HTTP_200_OK)
-        except :
+        except:
             return Response(status=status.HTTP_404_NOT_FOUND)
+    
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)
         serializers = MovieSerializers(movie,data = request.data)
@@ -55,6 +95,7 @@ def serial_movie_details(request, pk):
             return Response(serializers.data,status=status.HTTP_202_ACCEPTED)
         else:
             return Response(serializers.errors,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
